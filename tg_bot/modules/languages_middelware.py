97436aad48b885d8dfaf6f9eeddb3ae351dcf8bc
victor <vashatilov@edu.hse.ru>
2020-11-10 17:32:40 +0300

    add locales

--- tg_bot/modules/languages_middelware.py
+++ tg_bot/modules/languages_middelware.py
@@ -28,3 +28,26 @@ def setup_middleware(dp):
     i18n = ACLMiddleware(I18N_DOMAIN, LOCALES_DIR)
     dp.middleware.setup(i18n)
     return i18n
+
+#
+# Название - testbot, можете сменить на любое другое
+#
+# Запускаем первый раз
+# 1. Вытаскиваем тексты из файлов (он сам находит)
+# pybabel extract . -o locales/testbot.pot
+# 2. Создаем папку для перевода на английский
+# pybabel init -i locales/testbot.pot -d locales -D testbot -l en
+# 3. То же, на русский
+# pybabel init -i locales/testbot.pot -d locales -D testbot -l ru
+# 4. То же, на украинский
+# pybabel init -i locales/testbot.pot -d locales -D testbot -l uk
+# 5. Переводим, а потом собираем переводы
+# pybabel compile -d locales -D testbot
+#
+#
+# Обновляем переводы
+# 1. Вытаскиваем тексты из файлов, Добавляем текст в переведенные версии
+# pybabel extract . -o locales/testbot.pot
+# pybabel update -d locales -D testbot -i locales/testbot.pot
+# 3. Вручную делаем переводы, а потом Собираем
+# pybabel compile -d locales -D testbot
