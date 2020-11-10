eb9ecc339b89999646a3bc87cf52b2f5edab7fbb
Nikita <semaev2014@yandex.ru>
2020-11-10 14:06:12 +0300

    Added model schema for users

--- tg_bot/db/models.py
+++ tg_bot/db/models.py
@@ -1,9 +1,21 @@
+import asyncio
 from ..config import db
 
 
 class BotUser(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer(), primary_key=True)
-    tg_id = db.Column(db.BigInteger(), default=0)
-    language = db.Column(db.Unicode(), max_length=2, default="ru")
+    id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
+    tg_id = db.Column(db.BigInteger())
+    token = db.Column(db.Unicode(), default="", max_length=256)
+    lang = db.Column(db.Unicode(), max_length=2, default="ru")
+
+async def main():
+    await db.set_bind('postgresql://localhost/gino')
+    await db.gino.create_all()
+
+    # further code goes here
+
+    await db.pop_bind().close()
+
+asyncio.get_event_loop().run_until_complete(main())
