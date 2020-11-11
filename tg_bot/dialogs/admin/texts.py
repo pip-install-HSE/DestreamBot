d5c2752330965469646cf7dc0eb9c8fdc0e1e46c
Nikita <semaev2014@yandex.ru>
2020-11-12 01:57:23 +0300

    Added 1 part of main menu functional(debug 007) + new text in menu (002)

--- tg_bot/dialogs/admin/texts.py
+++ tg_bot/dialogs/admin/texts.py
@@ -2,7 +2,7 @@ from ...load_all import _
 
 bot_user_start = lambda: _("Сходи на платформу, затем вернись и дай мне откуда-то токен")
 
-main_menu = lambda json_data: _("<b>ГЛАВНОЕ МЕНЮ</b>\n\nТебя зовут") + f"{json_data['username']}\n" + _("Твой баланс") + f"{json_data['balance']['balance']} {json_data['balance']['currency']}\n\n" + _("История донатов\n") + f"<ссылка не подъехала>"
+main_menu = lambda json_data: _("<b>ГЛАВНОЕ МЕНЮ</b>\n\nТебя зовут") + f"{json_data['username']}\n" + _("Твой баланс") + f"{json_data['balance']['balance']} {json_data['balance']['currency']}\n\n" + _("История донатов\n") + f"!ссылка не подъехала!"
 error_token = lambda: _("Извините, но токен неправильный, попробуйте отправить снова")
 add_group = lambda: _("Добавьте, пожалуйста, бота в группу и передайте ему права администратора")
 notifications = lambda: _("Отлично, бот добавлен, теперь скажите, хотите ли Вы сообщать в группе о новых донатах")
