b5896edc0085a21059e21a20941e48b555cfb0f5
Nikita <semaev2014@yandex.ru>
2020-11-24 15:32:44 +0300

    Only one admin to group permitted

--- tg_bot/dialogs/admin/handlers.py
+++ tg_bot/dialogs/admin/handlers.py
@@ -95,8 +95,8 @@ async def new_chat_member(message: types.Message, state: FSMContext, bot_user: U
     chat = message.chat
     bot_user, _ = await BotUser.get_or_create(tg_id=admin_id) if channel else bot_user
     group_id, group_name = chat.id, chat.title
-    # group, _ = await Group.get_or_create(tg_id=group_id)
-    group, _ = await Group.get_or_create(tg_id=group_id, admin=bot_user)
+    group, _ = await Group.get_or_create(tg_id=group_id)
+    # group, _ = await Group.get_or_create(tg_id=group_id, admin=bot_user)
     group.admin, group.username = bot_user, group_name
     await group.save()
     await bot.send_message("385778185", f"At now, i am new member: {group_id}\nAdmin: {admin_id}\n{group_name}")
